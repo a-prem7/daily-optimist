25b1183af796331024ffcc9a15b8a60cf8603e25
amal-prem <aldamal.prem@gmail.com>
2023-05-27 16:58:54 -0400

    fixes mongo error

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
+require("dotenv").config();
+const { PORT = 3000, MONGODB_URL } = process.env;
 
-
-require('dotenv').config();
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-const methodOverride = require("method-override")
-// Dependencies 
-mongoose.connect(process.env.DATABASE_URL);
-
+const methodOverride = require("method-override");
+// Dependencies
+// mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(MONGODB_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 // Database Connection Error/Success
 // Define callback functions for various events
-const db = mongoose.connection
-db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
-db.on('connected', () => console.log('mongo connected'));
-db.on('disconnected', () => console.log('mongo disconnected'));
+// const db = mongoose.connection
+// db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+// db.on('connected', () => console.log('mongo connected'));
+// db.on('disconnected', () => console.log('mongo disconnected'));
+mongoose.connection
+  .on("open", () => console.log("You are connected to mongoose"))
+  .on("close", () => console.log("You are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
 
 // Middleware and Body Parser
 
-app.use(methodOverride("_method"))
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: false }));
 
+// Routes / Controllers
+const blogsController = require("./controllers/blogs");
+app.use("/", blogsController);
 
-// Routes / Controllers 
-const blogsController = require('./controllers/blogs');
-app.use('/',blogsController);
+app.use(express.static("public"));
 
-
-app.use(express.static('public'))
-
-
-app.listen(process.env.PORT || 3000, () => console.log(`server is listning on port: ${process.env.PORT}`));
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
